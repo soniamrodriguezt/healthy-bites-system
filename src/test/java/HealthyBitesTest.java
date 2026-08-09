0f4f283e05607cde96bfc3002035fa28f955ffed
soniamrodriguezt <sonia.mrdzt@gmail.com>
2026-08-09 06:07:20 -0600

    Agregar prueba JUnit para Healthy Bites

---
+++ src/test/java/HealthyBitesTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HealthyBitesTest {
+
+    @Test
+    public void pruebaSistemaHealthyBites() {
+        boolean sistemaActivo = true;
+        assertTrue(sistemaActivo);
+    }
+}
